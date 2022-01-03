82512550c4f0c2ddfffb2e4558e6f93e95945347
Craxtic LLC <88998069+DeathsploitDev@users.noreply.github.com>
2022-01-03 03:34:28 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask,  render_template
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def main():
+    vv = render_template('index.html')
+    return vv
+    
+        
+
+if __name__ == '__main__':
+    app.run()
