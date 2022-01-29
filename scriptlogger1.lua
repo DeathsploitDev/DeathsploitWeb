f2a86484a686c6c0cb2cd534dae9adfd8323baea
Craxtic LLC <88998069+DeathsploitDev@users.noreply.github.com>
2022-01-29 04:54:13 -0600

    Update and rename scriptlogger.lua to scriptlogger1.lua

--- scriptlogger1.lua
+++ scriptlogger1.lua
@@ -1,25 +1,25 @@
 local OSTime = os.time();
 local Time = os.date('!*t', OSTime);
 local Avatar = 'https://cdn.discordapp.com/embed/avatars/4.png';
-local Content = '@WebhookPing Someone has injected ZenEx into a game.';
+local Content = '@here Someone has injected NTG into a game.';
 local Embed = {
-    title = 'ZenEx Logger';
+    title = 'NTG Hub Logger';
     color = '99999';
     footer = { text = game.JobId };
     author = {
-        name = 'ROBLOX';
+        name = 'Roblox Script Injection Logger';
         url = 'https://www.roblox.com/';
     };
     fields = {
         {
             name = '{user}';
-            value = 'A user has Injected ZenEx into a game.';
+            value = 'A user has Injected the NTG Script into a game.';
         }
     };
     timestamp = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec);
 };
 (syn and syn.request or http_request) {
-    Url = 'https://discord.com/api/webhooks/936732000696549426/C_r_J3NPNrNo5GURMq1gHfrNW5enVdzMFglM2GonXXsywpIYJlRh51RXJDLv26zqHv3J';
+    Url = 'https://discord.com/api/webhooks/936936870233915453/Nv_IY8WxAyViUGZIdb37mNiGXGB0nYD3Ozbu23zvVRhvxrO7sujxu0PIKpyhujIRXqZf';
     Method = 'POST';
     Headers = {
         ['Content-Type'] = 'application/json';
