4a75cff544d0fa609a9874d6a5563149a7ad3d43
Craxtic LLC <88998069+DeathsploitDev@users.noreply.github.com>
2022-01-29 05:00:38 -0600

    Update scriptlogger1.lua

--- scriptlogger1.lua
+++ scriptlogger1.lua
@@ -1,9 +1,9 @@
 local OSTime = os.time();
 local Time = os.date('!*t', OSTime);
 local Avatar = 'https://cdn.discordapp.com/embed/avatars/4.png';
-local Content = '@here Someone has injected NTG into a game.';
+local Content = '@here Someone has injected ZenEx into a game.';
 local Embed = {
-    title = 'NTG Hub Logger';
+    title = 'ZenEx Hub Logger';
     color = '99999';
     footer = { text = game.JobId };
     author = {
@@ -12,14 +12,14 @@ local Embed = {
     };
     fields = {
         {
-            name = '{user}';
+            name = '';
             value = 'A user has Injected the NTG Script into a game.';
         }
     };
     timestamp = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec);
 };
 (syn and syn.request or http_request) {
-    Url = 'https://discord.com/api/webhooks/936936870233915453/Nv_IY8WxAyViUGZIdb37mNiGXGB0nYD3Ozbu23zvVRhvxrO7sujxu0PIKpyhujIRXqZf';
+    Url = 'https://discord.com/api/webhooks/936732000696549426/C_r_J3NPNrNo5GURMq1gHfrNW5enVdzMFglM2GonXXsywpIYJlRh51RXJDLv26zqHv3J';
     Method = 'POST';
     Headers = {
         ['Content-Type'] = 'application/json';
