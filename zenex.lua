f4e9232c484607317cf526035af5f70595026d62
Craxtic LLC <88998069+DeathsploitDev@users.noreply.github.com>
2022-01-28 21:31:09 -0600

    Create zenex.lua

---
+++ zenex.lua
@@ -0,0 +1,198 @@
+-- Burger King Destroyer, Credits: Craxtic#0001
+
+print("Hello! if you're seeing this, you've injected the ZenEx UI. Since you purchased. I'd like you to know the money goes towards the websites we host. Also, do NOT leak this script. It will be an instant blacklist and will be permanent.")
+
+wait(2)
+
+local NTG = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_2 = Instance.new("UICorner")
+local Gradient = Instance.new("Frame")
+local UIGradient = Instance.new("UIGradient")
+local UICorner_3 = Instance.new("UICorner")
+local _1 = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local _6 = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local _7 = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local _4 = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local _8 = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local _9 = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local _5 = Instance.new("TextButton")
+local UICorner_10 = Instance.new("UICorner")
+local _10 = Instance.new("TextButton")
+local UICorner_11 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+
+--Properties:
+
+NTG.Name = "NTG"
+NTG.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+Frame.Parent = NTG
+Frame.BackgroundColor3 = Color3.fromRGB(104, 67, 141)
+Frame.Position = UDim2.new(0.233532935, 0, 0.359807462, 0)
+Frame.Size = UDim2.new(0, 391, 0, 34)
+Frame.Active = true
+Frame.Draggable = true
+
+UICorner.Parent = Frame
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(118, 185, 232)
+TextLabel.Position = UDim2.new(0.00255754474, 0, -0.00757553987, 0)
+TextLabel.Size = UDim2.new(0, 390, 0, 25)
+TextLabel.Font = Enum.Font.LuckiestGuy
+TextLabel.Text = "(DPA) ZenEx UI                   bkroblox.xyz"
+TextLabel.TextColor3 = Color3.fromRGB(62, 255, 139)
+TextLabel.TextSize = 14.000
+
+UICorner_2.Parent = TextLabel
+
+Gradient.Name = "Gradient"
+Gradient.Parent = Frame
+Gradient.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Gradient.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Gradient.BorderSizePixel = 0
+Gradient.Position = UDim2.new(0.00255754474, 0, 0.738081753, 0)
+Gradient.Size = UDim2.new(0, 390, 0, 265)
+
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(182, 141, 159)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(64, 96, 161))}
+UIGradient.Parent = Gradient
+
+UICorner_3.CornerRadius = UDim.new(0, 4)
+UICorner_3.Parent = Gradient
+
+_1.Name = "1"
+_1.Parent = Gradient
+_1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_1.Position = UDim2.new(0.0128755365, 0, 0.038274914, 0)
+_1.Size = UDim2.new(0, 146, 0, 26)
+_1.Font = Enum.Font.SourceSans
+_1.Text = "(Coming Soon)"
+_1.TextColor3 = Color3.fromRGB(0, 0, 0)
+_1.TextSize = 14.000
+UICorner_4.Parent = _1
+
+_6.Name = "6"
+_6.Parent = Gradient
+_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_6.Position = UDim2.new(0.411841094, 0, 0.41185984, 0)
+_6.Size = UDim2.new(0, 146, 0, 26)
+_6.Font = Enum.Font.SourceSans
+_6.Text = "(Coming Soon)"
+_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+_6.TextSize = 14.000
+
+UICorner_5.Parent = _6
+
+_7.Name = "7"
+_7.Parent = Gradient
+_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_7.Position = UDim2.new(0.411841094, 0, 0.036657691, 0)
+_7.Size = UDim2.new(0, 146, 0, 26)
+_7.Font = Enum.Font.SourceSans
+_7.Text = "(Coming Soon)"
+_7.TextColor3 = Color3.fromRGB(0, 0, 0)
+_7.TextSize = 14.000
+
+UICorner_6.Parent = _7
+
+_4.Name = "4"
+_4.Parent = Gradient
+_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_4.Position = UDim2.new(0.012875542, 0, 0.167655051, 0)
+_4.Size = UDim2.new(0, 146, 0, 26)
+_4.Font = Enum.Font.SourceSans
+_4.Text = "Fates Admin"
+_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+_4.TextSize = 14.000
+_4.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://deathsploit.xyz/custom1.lua"))()
+end)
+
+UICorner_7.Parent = _4
+
+_8.Name = "8"
+_8.Parent = Gradient
+_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_8.Position = UDim2.new(0.411841094, 0, 0.164420485, 0)
+_8.Size = UDim2.new(0, 146, 0, 26)
+_8.Font = Enum.Font.SourceSans
+_8.Text = "(Coming Soon)"
+_8.TextColor3 = Color3.fromRGB(0, 0, 0)
+_8.TextSize = 14.000
+
+UICorner_8.Parent = _8
+
+_9.Name = "9"
+_9.Parent = Gradient
+_9.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_9.Position = UDim2.new(0.411841094, 0, 0.28840974, 0)
+_9.Size = UDim2.new(0, 146, 0, 26)
+_9.Font = Enum.Font.SourceSans
+_9.Text = "(Coming Soon)"
+_9.TextColor3 = Color3.fromRGB(0, 0, 0)
+_9.TextSize = 14.000
+
+UICorner_9.Parent = _9
+
+_5.Name = "5"
+_5.Parent = Gradient
+_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_5.Position = UDim2.new(0.012875542, 0, 0.290566027, 0)
+_5.Size = UDim2.new(0, 146, 0, 26)
+_5.Font = Enum.Font.SourceSans
+_5.Text = "click then press f9"
+_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+_5.TextSize = 14.000
+_5.MouseButton1Down:connect(function()
+	print("This script is made by Craxtic#0001, after injecting this script you accept the TOS. Please note: You could be banned for this. So we recommend an ALTBOT. Thanks!")
+end)
+UICorner_10.Parent = _5
+
+_10.Name = "10"
+_10.Parent = Gradient
+_10.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_10.Position = UDim2.new(0.0118410885, 0, 0.415094346, 0)
+_10.Size = UDim2.new(0, 146, 0, 26)
+_10.Font = Enum.Font.SourceSans
+_10.Text = "Chat Spammer"
+_10.TextColor3 = Color3.fromRGB(0, 0, 0)
+_10.TextSize = 14.000
+_10.MouseButton1Down:connect(function()
+while true do wait(2) 
+
+	local A_1 = "Hello! Do you want to stop the raids completely? DRP announces RAIDSTOP a product that allows you to INSTANTLY stop any raid for a certain amount of time. see droppurity.xyz for more info." local A_2 = "All" 
+	local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2) end
+end)
+UICorner_11.Parent = _10
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.Position = UDim2.new(0.0741688013, 0, 5.26470613, 0)
+TextLabel_2.Size = UDim2.new(0, 332, 0, 15)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "Note: This GUI automatically updates based on recommended scripts in the discord."
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextSize = 11.000
+
+TextLabel_3.Parent = NTG
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.Position = UDim2.new(0.297405183, 0, 0.593261123, 0)
+TextLabel_3.Size = UDim2.new(0, 200, 0, 25)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "Developed by Craxtic#0001 any issues? DM me."
+TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.TextSize = 14.000
