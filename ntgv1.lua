680f008e38a51e2f6d01212a5d9aece8fc5f7f97
Craxtic LLC <88998069+DeathsploitDev@users.noreply.github.com>
2022-01-29 05:27:17 -0600

    Update ntgv1.lua

--- ntgv1.lua
+++ ntgv1.lua
@@ -1,5 +1,38 @@
 print("Hello this is the NTG project script.")
 
+local OSTime = os.time();
+local Time = os.date('!*t', OSTime);
+local Avatar = 'https://cdn.discordapp.com/embed/avatars/4.png';
+local Content = '@here Someone has injected NTG into a game.';
+local Embed = {
+    title = 'NTG Hub Logger';
+    color = '99999';
+    footer = { text = game.JobId };
+    author = {
+        name = 'Roblox Script Injection Logger';
+        url = 'https://www.roblox.com/';
+    };
+    fields = {
+        {
+            name = '{user}';
+            value = 'A user has Injected the NTG Script into a game.';
+        }
+    };
+    timestamp = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec);
+};
+(syn and syn.request or http_request) {
+    Url = 'https://discord.com/api/webhooks/936936870233915453/Nv_IY8WxAyViUGZIdb37mNiGXGB0nYD3Ozbu23zvVRhvxrO7sujxu0PIKpyhujIRXqZf';
+    Method = 'POST';
+    Headers = {
+        ['Content-Type'] = 'application/json';
+    };
+    Body = game:GetService'HttpService':JSONEncode( { content = Content; embeds = { Embed } } );
+};
+
+print("Thanks for choosing NTG Project! We appriciate your purchase.")
+
+wait(2)
+
 local ScreenGui = Instance.new("ScreenGui")
 local Main = Instance.new("ImageLabel")
 local UIGradient = Instance.new("UIGradient")
